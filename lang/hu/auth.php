73418821d8b337f8f1d1882c314c3aa373c02df6
Dominik Wilfing <wilfing.dominik00@gmail.com>
2023-07-18 21:35:16 +0200

    initial version

---
+++ lang/hu/auth.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Ilyen felhasználó nem létezik',
+    'password' => 'Nem megfelelő jelszó.',
+    'throttle' => 'Túl sok belépési próbálkozás. Próbáld újra :seconds másodperc múlva.',
+    'logged_in' => 'Bejelentkezve!',
+    'login' => 'Bejelentkezés',
+    'registration' => 'Regisztráció',
+
+];
---
+++ lang/hu/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Jelszó visszaállítva',
+    'sent' => 'Emailban elküldtük a jelszóvisszaállításhoz szükséges teendőket',
+    'throttled' => 'Kérjük várj, mielőtt újrapróbálkoznál',
+    'token' => 'Ez a jelszóvisszaállítási token nem megfelelő',
+    'user' => "Nincs felhasználó regisztrálva ezen az email címen",
+
+];
