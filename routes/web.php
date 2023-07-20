1ec2d31bb530dc1c9f90faa3fb1d0d6b14f492a3
Dominik Wilfing <wilfing.dominik00@gmail.com>
2023-07-20 20:26:35 +0200

    "/" route now redirects to dashboard when logged in

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,9 @@ use App\Http\Controllers\SettingsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    if (auth()->check()) {
+        return redirect('dashboard');
+    }
     return view('welcome');
 });
 
