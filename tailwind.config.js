5c028d45ca23ffe4a18997ff8602b42744c9c7f3
Dominik Wilfing <wilfing.dominik00@gmail.com>
2023-07-21 17:24:56 +0200

    updated tailwind
    - and changed config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,8 @@
-import defaultTheme from "tailwindcss/defaultTheme";
-import forms from "@tailwindcss/forms";
-
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
-        "./storage/framework/views/*.php",
-        "./resources/views/**/*.blade.php",
-        "./resources/views/dashboard.blade.php",
-    ],
-
+    content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
-            },
-        },
+        extend: {},
     },
-
-    plugins: [forms],
+    plugins: [],
 };
