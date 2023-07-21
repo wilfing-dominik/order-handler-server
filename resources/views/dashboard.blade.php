ee769d9d1511c14c8fe2c89ed6b8a2c0fcdcb8bf
Dominik Wilfing <wilfing.dominik00@gmail.com>
2023-07-21 23:14:09 +0200

    restyled dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,23 +5,29 @@
         </h2>
     </x-slot>
 
-    <div class="flex flex-row">
-        <div class="flex gap-4">
+    <div class="flex flex-row gap-4" style="height: 75vh">
+        <div class="flex flex-wrap justify-center gap-4 w-7/12 items-start pt-10">
             @foreach ($tables as $table)
-                <a id="{{ $table->id }}" class="tableCard bg-white p-3">
+                <a id="{{ $table->id }}" class="tableCard flex justify-center items-center bg-white w-52 h-28 flex-col rounded cursor-pointer shadow-sm">
                     <div>{{ $table->name }}</div>
                     <div>1350 Ft</div>
                 </a>
             @endforeach
         </div>
 
-        <div class="">
-            <div class="orderContainer">
-                Kattintson egy asztalra a rendelések megjelenítéséhez!
+        <div class="flex flex-col gap-4 w-5/12 h-full">
+            <div class="h-full">
+                <p class="text-xl font-semibold pb-3">Rendelések</p>
+                <div class="orderContainer bg-white shadow-sm p-4 rounded-sm h-full">
+                    Kattintson egy asztalra a rendelések megjelenítéséhez!
+                </div>
             </div>
 
-            <div class="productContainer">
-            </div>
+            {{-- <div class="">
+                <p class="text-xl font-semibold">Terméklista</p>
+                <div class="productContainer bg-white shadow-sm p-4 rounded-sm">
+                </div>
+            </div> --}}
         </div>
     </div>
 
@@ -60,27 +66,27 @@
                     })
                 });
 
-                axios.get('/get-products')
-                .then(function (response) {
-                    const productContainer = document.querySelector(".productContainer")
-                    for (let i = 0; i < response.data.length; i++) {
-                        const name = document.createElement('p');
-                        name.textContent = `${response.data[i].name}`;
-                        productContainer.appendChild(name);
+                // FETCH PRODUCT LIST
+                // axios.get('/get-products')
+                // .then(function (response) {
+                //     const productContainer = document.querySelector(".productContainer")
+                //     for (let i = 0; i < response.data.length; i++) {
+                //         const name = document.createElement('p');
+                //         name.textContent = `${response.data[i].name}`;
+                //         productContainer.appendChild(name);
 
-                        const price = document.createElement('p');
-                        price.textContent = `${response.data[i].price}`;
-                        productContainer.appendChild(price);
-                    }
-                })
-                .catch(function (error) {
-                    const productContainer = document.querySelector(".productContainer")
-                    const paragraph = document.createElement('p');
-                    paragraph.textContent = `Hiba az adat lekérésekor`;
-                    productContainer.appendChild(paragraph);
-                });
+                //         const price = document.createElement('p');
+                //         price.textContent = `${response.data[i].price}`;
+                //         productContainer.appendChild(price);
+                //     }
+                // })
+                // .catch(function (error) {
+                //     const productContainer = document.querySelector(".productContainer")
+                //     const paragraph = document.createElement('p');
+                //     paragraph.textContent = `Hiba az adat lekérésekor`;
+                //     productContainer.appendChild(paragraph);
+                // });
             })
         </script>
     @endsection
-
 </x-app-layout>
