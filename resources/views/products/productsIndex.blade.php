2e83b26f9842954acc8940bd86af5826033628cf
Dominik Wilfing <wilfing.dominik00@gmail.com>
2023-07-23 19:02:43 +0200

    a product now acts as a button for edit

--- resources/views/products/productsIndex.blade.php
+++ resources/views/products/productsIndex.blade.php
@@ -11,7 +11,7 @@
 
     <div class="flex gap-4 flex-col max-w-7xl mx-auto">
         @foreach ($products as $product)
-                    <div class="flex flex-row bg-white shadow justify-between px-6 py-4 rounded">
+                    <a href="{{ route('products.edit', ['product' => Str::ascii($product->name)]) }}" class="flex flex-row bg-white shadow justify-between px-6 py-4 rounded">
                         <div class="flex justify-start items-center w-full">{{ $product->name }}</div>
 
                         <div class="flex justify-center items-center w-full">{{ $product->price }}</div>
@@ -23,15 +23,15 @@
                         @endif
 
                         <div class="flex flex-row gap-4">
-                            <x-primary-link-button  href="{{ route('products.edit', ['product' => Str::ascii($product->name)]) }}" >
+                            <div class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150" >
                                 {{ __('products.edit') }}
-                            </x-primary-link-button>
+                            </div>
 
                             {{-- <x-primary-link-button href="{{ route('products.destroy', ['product' => Str::ascii($product->id)]) }}">
                                 {{ __('products.destroy') }}
                             </x-primary-link-button> --}}
                         </div>
-                    </div>
+                    </a>
         @endforeach
     </div>
 
